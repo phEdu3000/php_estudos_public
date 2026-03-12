52117c53c27fbde0e18039bedc3d552e4a74adfb
Raphael Eduardo Ferreira de Oliveira <eduardorafael275@gmail.com>
2026-03-12 14:13:17 -0300

    Update Php_Par_ou_Impar.php

--- Php-estudo/Php_Par_ou_Impar.php
+++ Php-estudo/Php_Par_ou_Impar.php
@@ -1,26 +1,35 @@
-
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Par ou Ímpar</title>
 </head>
 <body>
-    <?php
 
-function checkNumber($num) {
-    if ($num % 2 == 0) {
-        return "Par (Even)";
+<form method="post">
+    Digite um número:
+    <input type="number" name="numero">
+    <button type="submit">Verificar</button>
+</form>
+
+<?php
+
+function checkNumber($number){
+    if ($number % 2 == 0){
+        return "Par";
     } else {
-        return "Impar (Odd)";
+        return "Ímpar";
     }
 }
 
-// Example usage
-$number = 10;
-echo "O número $number é: " . checkNumber($number);
+if(isset($_POST['numero'])){
+    $numero = $_POST['numero'];
+    echo "O número $numero é: " . checkNumber($numero);
+}
 
 ?>
+
 </body>
 </html>
+
