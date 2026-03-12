b8376a0c443cbfe2c8e72db505189a194035a0da
Raphael Eduardo Ferreira de Oliveira <eduardorafael275@gmail.com>
2026-03-12 14:55:04 -0300

    Calculadora Simples if e else
    Cálculos simples

---
+++ Php-estudo/Calculadora_Simples.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculadora Simples</title>
+</head>
+<body>
+<h2>Calculadora Simples</h2>
+<form method="post">
+
+<input type="number" name="num1" required>
+<br><br>
+<input type="number" name="num2" required>
+<br><br>
+<select name="operacao">
+    <option value="soma">+</option>
+    <option value=subtracao>-</option>
+    <option value=multiplicacao>*</option>
+    <option value=divisao>/</option>
+</select>
+<br><br>
+
+<button type= "submit">calcular</button>
+</form>
+    
+<?php
+if(isset($_POST['num1'])&& isset($_POST['num2'])){
+    $num1 = $_POST['num1'];
+    $num2 = $_POST['num2'];
+    $operacao = $_POST['operacao'];
+
+    
+    if($operacao == "soma"){
+        $resultado = $num1 + $num2;
+        }
+        elseif($operacao == "subtracao"){
+            $resultado = $num1 - $num2;
+        }
+        elseif($operacao == "multiplicacao"){
+            $resultado = $num1 * $num2;
+        }
+        elseif($operacao == "divisao"){
+            if($num2 >= 1){
+                $resultado = $num1 / $num2;
+            }else{
+                echo "não é possivel dividir por zero ou negativo";
+            }
+        }
+    }
+?>
+</body>
+</html>
