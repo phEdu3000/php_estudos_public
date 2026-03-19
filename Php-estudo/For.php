24ae2fdadc570a22f1c8ce713acf8b32738390e3
Raphael Eduardo Ferreira de Oliveira <eduardorafael275@gmail.com>
2026-03-19 11:35:44 -0300

    Estudos For 01

---
+++ Php-estudo/For.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Loops for</title>
+</head>
+<body>
+    <h1>Exercicio 01-for</h1>
+        <p>
+            <?php
+            //printar numeros de 1 a 5 usando o for 
+            for($i=0;$i<5;$i++){ 
+                  echo ($i+1);
+                  echo"<br>";
+            }
+            ?>
+
+        </p>    
+
+
+</body>
+</html>
