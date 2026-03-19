a9406b30efbac0b8bc92dd1b3711ebed9575a9fd
Raphael Eduardo Ferreira de Oliveira <eduardorafael275@gmail.com>
2026-03-19 11:36:39 -0300

    While 02

---
+++ Php-estudo/while02.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Loops While</title>
+</head>
+<body>
+        <h1>Exercicio 02-while</h1>
+
+
+    <p>
+    <?php
+    $i= 10;
+    while ($i >= 0){
+        echo"$i<br>"; 
+        $i--;
+    }
+    ?>
+
+
+    </p>
+</body>
+</html>
