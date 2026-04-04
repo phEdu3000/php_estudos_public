0785236af6ac09abef7314b8df621bb351bc6909
Raphael Eduardo Ferreira de Oliveira <eduardorafael275@gmail.com>
2026-04-04 00:19:49 -0300

    Update Simulação_Caixa_Eletronico.php

--- Php-estudo/Simulação_Caixa_Eletronico.php
+++ Php-estudo/Simulação_Caixa_Eletronico.php
@@ -31,42 +31,37 @@
         
     }
 
-
-echo "🏧 Caixa Eletrônico\n";
-
-
-
     switch ($opcao) {
 
         case 1:
-            echo "💰 Saldo atual: R$ " . number_format($saldo, 2) . "\n";
+            echo "💰 Saldo atual: R$ " . number_format($_SESSION['saldo'], 2,',','.') . "\n";
             break;
 
         case 2:
-            $valor = (float) readline("Digite o valor do depósito: ");
-            
-            if ($valor > 0) {
-                $saldo += $valor;
-                echo "✅ Depósito realizado!\n";
-            } else {
-                echo "❌ Valor inválido\n";
-            }
-            break;
+            echo '
+            <form method="post">
+                <input type= "hidden" name = "opcao" value="depositar">
+                <input type= "number" step="0.01" name="valor" placeholder="Valor do depósito">';
+            echo '<button type="submit">Depositar</button>';
+            echo '</form>';
+                break;
+            case 3:
+            echo'<form method="post">
+                <input type= "hidden" name = "opcao" value="sacar">
+                <input type= "number" step="0.01" name="valor" placeholder="Valor do saque">;
+             <button type="submit">Sacar</button>;
+             </form>';
+             break;
 
-        case 3:
-            $valor = (float) readline("Digite o valor do saque: ");
+             case"depositar":
+             $valor = $_POST['valor'];
 
-            if ($valor <= 0) {
-                echo "❌ Valor inválido\n";
-
-            } elseif ($valor > $saldo) {
-                echo "🚫 Saldo insuficiente!\n";
-
-            } else {
-                $saldo -= $valor;
-                echo "💸 Saque realizado!\n";
-            }
-            break;
+             if($valor <= 0){
+                echo "❌ Valor inválido para depósito\n";
+             } else {
+                $_SESSION['saldo'] += $valor;
+                echo "✅ Depósito de R$ " . number_format($valor, 2,',','.') . " realizado com sucesso!\n";
+             }
 
         case 4:
             echo "👋 Encerrando...\n";
