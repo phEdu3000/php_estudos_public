03cc9a2483e95223e24649095897f15fecc56ee4
Raphael Eduardo Ferreira de Oliveira <eduardorafael275@gmail.com>
2026-04-16 20:08:42 -0300

    Estudos

---
+++ Php-estudo/array_push.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+$numeros = [1, 2, 3];
+
+array_push($numeros, 4);
+
+print_r($numeros);
+?>
+</body>
+</html>
---
+++ Php-estudo/soma.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+$a = 10;
+$b = 5;
+
+$soma = $a + $b;
+
+echo $soma;
+?>
+</body>
+</html>
